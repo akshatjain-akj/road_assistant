4f64b7a7aa8865c38ff7684e849b5cc9f39b9dcd
Akshat jain <72062559+akshatjain-akj@users.noreply.github.com>
2021-12-05 21:41:17 +0530

    Add files via upload

---
+++ road/seller.php
@@ -0,0 +1,19 @@
+<?php
+include('database.php');
+header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
+    header("Content-type:application/json");
+$SQL = "SELECT s.name as name,l.city as city,l.state as state,p.type as type FROM seller s,location l,parts p WHERE s.locid=l.id and s.parts=p.id";
+$exeSQL = mysqli_query($con, $SQL);
+$check =  mysqli_num_rows($exeSQL);
+
+$response[]=array();
+    while($row = mysqli_fetch_array($exeSQL))
+    {
+        $response[]=array("name"=>$row['name'],"city"=>$row['city'],"state"=>$row['state'],"type"=>$row['type']);
+    }
+array_shift($response);
+
+echo json_encode($response);
+?>
