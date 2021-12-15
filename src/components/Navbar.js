0d31610c4eb9abc026802abb8016bda16df89e68
copyNinja1219 <thapacyrus30@gmail.com>
2021-12-15 13:00:01 +0530

    added state and city in seller.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,13 +10,8 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
 import SupportSharpIcon from '@mui/icons-material/SupportSharp';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 
 // import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 
 const Navbar = () => {
   const [age, setAge] = React.useState('');
@@ -46,40 +41,6 @@ const Navbar = () => {
                 <Button color="inherit">RoadAssist </Button>
               </Link>
             </Typography>
-
-            <Link
-              to="/mechanic/show"
-              style={{ color: '#fff', marginTop: '0px' }}
-            >
-              <Box
-                component="form"
-                sx={{
-                  '& > :not(style)': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-              >
-                <TextField
-                  id="standard-basic"
-                  label="State"
-                  variant="standard"
-                />
-                <TextField
-                  id="standard-basic"
-                  label="City"
-                  variant="standard"
-                />
-                {/* <Link to="/login" style={{ color: '#fff', marginTop: '0px' }}> */}
-                <Button
-                  color="inherit"
-                  style={{ color: '#fff', marginTop: '15px' }}
-                  variant="outlined"
-                >
-                  Find{' '}
-                </Button>
-                {/* </Link> */}
-              </Box>
-            </Link>
 
             <Link
               to="/mechanic/show"
--- src/components/seller/Sellers.js
+++ src/components/seller/Sellers.js
@@ -7,20 +7,77 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import Card from '@mui/material/Card';
 import axios from 'axios';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+
+// import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+// import Box/ from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+// import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 
 const PATH = 'http://localhost/road/seller.php';
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 export default class sellers extends Component {
   constructor() {
     super();
 
     this.state = {
-      username:'',
+      username: '',
       sell: [],
-      stop:'no',
+      stop: 'no',
     };
     this.seller_data = this.seller_data.bind(this);
     // this.seller = this.seller.bind(this);
@@ -35,41 +92,53 @@ export default class sellers extends Component {
       data: this.state,
     })
       .then((result) => {
-        if(this.state.stop=='no')
-        {
+        if (this.state.stop === 'no') {
           this.setState({
-            
             sell: result.data,
-            stop:'yes'
+            stop: 'yes',
           });
         }
-       
       })
       .catch((error) => this.setState({}));
   }
 
   componentDidMount = () => {
-
-    let username = localStorage.customername
-    if (username != undefined) {
+    let username = localStorage.customername;
+    if (username !== undefined) {
       this.setState({
-        username: JSON.parse(username)
+        username: JSON.parse(username),
       });
     }
     this.seller_data();
-
-
-  }
+  };
   componentDidUpdate = () => {
     this.seller_data();
     console.log(this.state);
-    
-
-  }
+  };
   Seller = () => {
     return (
       <div>
         <Container>
+          <Box sx={{ flexGrow: 1 }} style={{ marginTop: '10px' }}>
+            <AppBar position="static">
+              <Toolbar>
+                <TextField
+                  id="standard-basic"
+                  label="State"
+                  variant="standard"
+                />
+                <TextField
+                  id="standard-basic"
+                  label="City"
+                  variant="standard"
+                  style={{ color: '#fff', margin: '0px 40px' }}
+                />
+                <Button color="inherit" variant="outlined">
+                  Find{' '}
+                </Button>
+              </Toolbar>
+            </AppBar>
+          </Box>
           <Grid
             container
             spacing={2}
